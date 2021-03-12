1876b88e918261a7838c3a396ac2d8f62ae75f41
LP-Codes <lovelesh_p@yahoomail.com>
2021-03-12 19:32:39 +0530

    Updated

---
+++ Scripts/index.js
@@ -0,0 +1,57 @@
+
+
+  //   const apiKey = "0926e3715bd414c4bdd946435fb4553d";
+  //   const inputVal = b.city;
+
+  //   // added weather data api
+  //   const url4 = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+  //   fetch(url4)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       // console.warn(data);
+  //       let city1 = data;
+  //       document.getElementById("weather").innerHTML =
+  //         "Your City's Current temperature is : " +
+  //         city1.main.temp +
+  //         " Degree Celcius";
+  //     });
+  //   // console.log(b.ip);
+  // });
+
+  $(document).ready(function () {
+    // for getting date
+    $("#ip").prepend(` <h5> <i class="far fa-clock"></i> ${Date()}</h5>`);
+
+// geolocation api
+    url1 = "https://freegeoip.app/json/";
+    fetch(url1)
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data);
+        let results=data;
+        $("#geol").prepend( `<h5>Your Location Is Detected As</h5>`+`<h3> <i class="fas fa-street-view"></i> ${results.city +"-"+results.region_name}</h3>`);
+
+        const apiKey = "0926e3715bd414c4bdd946435fb4553d";
+        const inputVal = results.city;
+    
+        // added weather data api
+        const url4 = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+        fetch(url4)
+          .then(res => res.json())
+          .then(data => {
+             console.log(data);
+             $("#weather").prepend(`<h5>Current Weather at your location is :</h5>`+`<h4> <i class="fas fa-cloud-sun-rain"></i> ${data.main.temp +" <span>&#8451;</span>"}</h4>`);
+        });
+
+            
+
+    });
+   
+ 
+
+
+
+  });
+// get public ip,
+
+  
